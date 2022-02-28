74b8316ffe7e8ab7e036f7420a6fcd5b7bd32004
Abhishek Gupta <abhishek.gupta@highspot.com>
2022-02-27 17:57:35 -0800

    Disable session creation

--- src/main/java/com/highspot/authgateway/security/WebSecurityConfig.java
+++ src/main/java/com/highspot/authgateway/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 @Configuration
@@ -13,9 +14,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-
-                .anyRequest().
-                authenticated().and()
-        .csrf().disable().formLogin(Customizer.withDefaults()).httpBasic();
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.NEVER)
+                .and()
+                .csrf().disable().formLogin(Customizer.withDefaults()).httpBasic();
     }
 }
